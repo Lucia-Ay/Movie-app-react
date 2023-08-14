3df29660bddfce28ac6d4833652a1a30a847a1f8
Lucia Aydin <114146153+Lucia-Ay@users.noreply.github.com>
2023-08-14 23:17:53 +0300

    Add Home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import './Home.scss';
+import HeroBanner from './heroBanner/HeroBanner';
+import Popular from './popular/Popular';
+import TopRated from './topRated/TopRated';
+import Trending from './trending/Trending';
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <HeroBanner />
+      <Trending />
+      <Popular />
+      <TopRated />
+    </div>
+  );
+};
+
+export default Home;
